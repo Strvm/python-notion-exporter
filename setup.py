03723dd31dfa20452e6ea7b41dfca3688795ea4d
Romeo <romeo@echo-analytics.io>
2023-08-26 17:42:37 +0200

    feat: package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="python-python_notion_exporter",
+    name="python_notion_exporter",
     author="Roméo Phillips",
     author_email="phillipsromeo@gmail.com",
     description="Export and download Notion pages asynchronously",
@@ -20,7 +20,6 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     classifiers=[
-        # see https://pypi.org/classifiers/
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
@@ -34,15 +33,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
-    # install_requires=['Pillow'],
     extras_require={
         "dev": ["check-manifest"],
-        # 'test': ['coverage'],
     },
-    # entry_points={
-    #     "console_scripts": [  # This can provide executable scripts
-    #         "run=python_notion_exporter:main",
-    #         # You can execute `run` in bash to run `main()` in src/examplepy/__init__.py
-    #     ],
-    # },
 )
